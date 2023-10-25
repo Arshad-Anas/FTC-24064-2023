4644e98f72f89cd0b6fe2b83800e68124c27d964
TheRealRamo1234 <omarnebula14@gmail.com>
2023-10-25 18:50:55 -0400

    Revert "Removing old code"
    This reverts commit 739a05d16f796cfad2dfb49866fcca6a9a7f26a3.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
@@ -12,3 +12,32 @@ import org.firstinspires.ftc.teamcode.subsystems.MecanumDrivetrain;
 
 import java.util.List;
 
+@TeleOp(group = "21836 TeleOp")
+public class MainTeleOp extends LinearOpMode {
+
+    MultipleTelemetry myTelemetry;
+    List<LynxModule> hubs;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        myTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        MecanumDrivetrain drivetrain = new MecanumDrivetrain(hardwareMap, 537.7, 312);
+        GamepadEx Gamepad1 = new GamepadEx(gamepad1);
+        hubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule hub : hubs) hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            for (LynxModule hub : hubs) hub.clearBulkCache();
+            drivetrain.run(
+                    Gamepad1.getLeftX(),
+                    Gamepad1.getLeftY(),
+                    Gamepad1.getRightX()
+            );
+            myTelemetry.update();
+        }
+    }
+}
+//hello kashif
