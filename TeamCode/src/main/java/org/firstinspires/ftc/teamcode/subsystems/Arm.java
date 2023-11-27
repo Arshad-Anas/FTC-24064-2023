057d77f71f0d293a025dd77e5bdccf10ad905339
SquidCooki2 <83745822+SquidCooki2@users.noreply.github.com>
2023-11-26 22:14:55 -0500

    Added PID controlling to the arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,28 +1,79 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
+import static com.arcrobotics.ftclib.hardware.motors.Motor.GoBILDA.RPM_117;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.toRadians;
+
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.control.State;
+import org.firstinspires.ftc.teamcode.control.controllers.PIDController;
+import org.firstinspires.ftc.teamcode.control.filters.FIRLowPassFilter;
+import org.firstinspires.ftc.teamcode.control.gainmatrices.LowPassGains;
+import org.firstinspires.ftc.teamcode.control.gainmatrices.PIDGains;
 
 public class Arm {
 
-    private Servo carriageServo;
+    public static PIDGains pidGains = new PIDGains(
+            0,
+            0,
+            0,
+            Double.POSITIVE_INFINITY
+    );
 
-    private DcMotor masterMotor, slaveMotor;
+    public static LowPassGains filterGains = new LowPassGains(0, 2);
 
-    private GamepadEx gamepad2;
+    // TODO Measure
+    public static double
+            DEPOSIT_POSITION = 0,
+            COLLECT_POSITION = 0,
+            TICKS_TO_DEGREES = 0,
+            K_COS = 0;
+
+    private final MotorEx motor;
+
+    private final FIRLowPassFilter derivFilter = new FIRLowPassFilter(filterGains);
+    private final PIDController controller = new PIDController(derivFilter);
+
+    private State currentState = new State();
+
+    private boolean isExtended = false;
 
     public Arm (HardwareMap hardwareMap) {
-        carriageServo = hardwareMap.get(Servo.class, "cServo");
-        masterMotor = hardwareMap.get(DcMotor.class, "maMotor");
-        slaveMotor = hardwareMap.get(DcMotor.class, "slaMotor");
+        motor = new MotorEx(hardwareMap, "arm", RPM_117);
     }
 
-    public void deposit() {
-        carriageServo.setPosition(0.6);
+    public void extend() {
+        isExtended = true;
+    }
+
+    public void retract() {
+        isExtended = false;
+    }
+
+    public void run() {
+        currentState = new State(motor.encoder.getPosition());
+        controller.setGains(pidGains);
+        derivFilter.setGains(filterGains);
+
+        controller.setTarget(new State(isExtended ? DEPOSIT_POSITION : COLLECT_POSITION));
+        motor.set(
+                controller.calculate(currentState) +
+                        cos(toRadians(currentState.x * TICKS_TO_DEGREES)) * K_COS
+        );
+    }
+
+    public void printTelemetry(MultipleTelemetry telemetry) {
+        telemetry.addData("Arm is", (isExtended ? "" : "not") + " extended");
+    }
+
+    public void printNumericalTelemetry(MultipleTelemetry telemetry) {
+        telemetry.addData("Current position (ticks)", currentState.x);
+        telemetry.addData("Error derivative (ticks/s)", controller.getErrorDerivative());
+        telemetry.addData("Current angle (degrees)", currentState.x * TICKS_TO_DEGREES);
     }
 }
 
